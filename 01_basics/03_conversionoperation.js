5eb2f25cbac86de155f18d995406ed8ea144bd23
Salim Ansari <150717361+imsalimansari@users.noreply.github.com>
2025-09-12 06:17:53 +0000

    Operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,10 +1,10 @@
 let score = null
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log( valueInNumber); // But it is a NaN
+// console.log( valueInNumber); // But it is a NaN
 
 
 /* "33" => 33
@@ -17,7 +17,7 @@ console.log( valueInNumber); // But it is a NaN
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0=> false
 //"" => false
@@ -25,5 +25,12 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// *****************************Operation*****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
