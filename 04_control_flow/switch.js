751b586802ad5c9eb1c79f8494169292037b7091
Salim Ansari <150717361+imsalimansari@users.noreply.github.com>
2026-05-30 23:28:20 +0000

    Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,97 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log('January');
+//         break;
+//     case 2:
+//         console.log('February');
+//         break;
+//     case 3:
+//         console.log('March');
+//         break;
+//     case 4:
+//         console.log('April');
+//         break;
+//     case 5:
+//         console.log('May');
+//         break;
+//     case 6:
+//         console.log('June');
+//         break;
+//     case 7:
+//         console.log('July');
+//         break;
+//     case 8:
+//         console.log('August');
+//         break;
+//     case 9:
+//         console.log('September');
+//         break;
+//     case 10:
+//         console.log('October');
+//         break;
+//     case 11:
+//         console.log('November');
+//         break;
+//     case 12:
+//         console.log('December');
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month = 'Dec'
+
+// switch (month) {
+//     case 'Jan':
+//         console.log('January');
+//         break;
+//     case 'Feb':
+//         console.log('February');
+//         break;
+//     case 'Mar':
+//         console.log('March');
+//         break;
+//     case 'Apr':
+//         console.log('April');
+//         break;
+//     case 'May':
+//         console.log('May');
+//         break;
+//     case 'June':
+//         console.log('June');
+//         break;
+//     case 'July':
+//         console.log('July');
+//         break;
+//     case 'Aug':
+//         console.log('August');
+//         break;
+//     case 'Sept':
+//         console.log('September');
+//         break;
+//     case 'Oct':
+//         console.log('October');
+//         break;
+//     case 'Nov':
+//         console.log('November');
+//         break;
+//     case 'Dec':
+//         console.log('December');
+//         break;
+
+//     default:
+//         break;
+// }
