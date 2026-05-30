9598dad872609c652a393b52378fcffcc08179ed
Salim Ansari <150717361+imsalimansari@users.noreply.github.com>
2026-05-30 21:59:27 +0000

    Control Flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,25 @@
+// if (3 !== '3') {
+//     console.log('True'); 
+// }
+// else {
+//     console.log('False');
+// }
+
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = 'fly'
+//     console.log(`User Power: ${power}`);  
+// }
+//     console.log(`User Power: ${power}`);
+
+//**** Short Hand Notation ****/
+
+// const balance = 1000
+// if (balance > 500) console.log('test');
+                                                
+// const balance = 1000
+// if (balance > 500) console.log('test'),
+//     console.log('test2');
+    
