415ac2bde8da639fe640c54fb44972ad90467a0c
Salim Ansari <150717361+imsalimansari@users.noreply.github.com>
2026-05-31 00:14:39 +0000

    While and Do While Lopp

---
+++ 05_iterations/two.js
@@ -0,0 +1,13 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is: ${index}`);
+    index ++;
+}
+
+
+let score = 1;
+
+do{
+    console.log(`Score is: ${score}`);
+    score++;
+} while (score <= 10);
